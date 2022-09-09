5e5ee368d740f7d5893f6223c32541f019710e58
DominicHoiseth <dh634921@ohio.edu>
2022-09-09 17:57:05 -0400

    Full send hope this works

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *  File: git-lab-program.cc
+ *  Author: Dominic Hoiseth
+ *  Date: 9-9-2022
+ *  Description: C++ program for repository on github
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+
+ using namespace std;
+
+ int main(int argc, char const *argv[]) {
+
+     cout << "Hello Git!!" << endl;
+
+     return 0;
+ }
